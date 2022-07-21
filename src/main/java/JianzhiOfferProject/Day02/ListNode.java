489b6c168af00e4730194c4cd2116a3c63bd9bbf
Yining Wang <wangyining203@gmail.com>
2022-07-21 22:38:02 +0800

    fix structure

---
+++ src/main/java/JianzhiOfferProject/Day02/ListNode.java
@@ -0,0 +1,14 @@
+package JianzhiOfferProject.Day02;
+
+/**
+ * @author timwong5
+ * @date 2022-07-21 22:37
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
--- src/main/java/JianzhiOfferProject/Day02/Solution.java
+++ src/main/java/JianzhiOfferProject/Day02/Solution.java
@@ -28,11 +28,3 @@ public class Solution {
 
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-}
