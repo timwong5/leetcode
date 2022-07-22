c0470295fb3f45aa97d2dd7cc9995f33d41518a8
Yining Wang <wangyining203@gmail.com>
2022-07-23 00:50:48 +0800

    add Day03-2

--- src/main/java/JianzhiOfferProject/Day03/Solution2.java
+++ src/main/java/JianzhiOfferProject/Day03/Solution2.java
@@ -5,4 +5,14 @@ package JianzhiOfferProject.Day03;
  * @date 2022-07-23 0:40
  */
 public class Solution2 {
+    public String reverseLeftWords(String s, int n) {
+        String s1 = s.substring(0,n);
+        String s2 = s.substring(n,s.length());
+        return s2+s1;
+    }
+    //Todo 不申请额外空间 解法2
+    public String reverseLeftWords2(String s, int n) {
+        return s;
+    }
+
 }
