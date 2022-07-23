ad06049e9a8372aeaad6738dd9b83a5674afdfbe
Yining Wang <wangyining203@gmail.com>
2022-07-23 20:18:32 +0800

    change the project structs

---
+++ JianzhiOffer/src/main/java/JianzhiOfferProject/Day04/Solution.java
@@ -0,0 +1,11 @@
+package JianzhiOfferProject.Day04;
+
+/**
+ * @author timwong5
+ * @date 2022-07-23 20:14
+ */
+public class Solution {
+    public int findRepeatNumber(int[] nums) {
+        return nums[1];
+    }
+}
