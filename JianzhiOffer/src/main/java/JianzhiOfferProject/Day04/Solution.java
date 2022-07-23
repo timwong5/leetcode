0391af1d15cc0475323c9476aa395213941d091c
Yining Wang <wangyining203@gmail.com>
2022-07-23 21:34:35 +0800

    add Day04-1 findRepeatNumber

--- JianzhiOffer/src/main/java/JianzhiOfferProject/Day04/Solution.java
+++ JianzhiOffer/src/main/java/JianzhiOfferProject/Day04/Solution.java
@@ -1,11 +1,22 @@
 package JianzhiOfferProject.Day04;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author timwong5
  * @date 2022-07-23 20:14
  */
 public class Solution {
     public int findRepeatNumber(int[] nums) {
-        return nums[1];
+        int temp = 0;
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.add(nums[i]) == false){
+                temp = nums[i];
+                break;
+            }
+        }
+        return temp;
     }
 }
